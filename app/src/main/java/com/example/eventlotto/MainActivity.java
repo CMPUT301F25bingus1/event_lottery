ce32efae8aa91a2011c7fceed101d88f871d07bd
Yong Lee <gjftns7220@gmail.com>
2025-10-25 20:23:21 -0600

    ui backbone

--- app/src/main/java/com/example/eventlotto/MainActivity.java
+++ app/src/main/java/com/example/eventlotto/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,39 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        if (bottomNavigationView != null) {
+            bottomNavigationView.setOnItemSelectedListener(item -> {
+                int id = item.getItemId();
+                if (id == R.id.nav_home) {
+                    loadFragment(new com.example.eventlotto.ui.HomeFragment());
+                    return true;
+                } else if (id == R.id.nav_scan) {
+                    loadFragment(new com.example.eventlotto.ui.ScanFragment());
+                    return true;
+                } else if (id == R.id.nav_notifications) {
+                    loadFragment(new com.example.eventlotto.ui.NotificationsFragment());
+                    return true;
+                } else if (id == R.id.nav_my_events) {
+                    loadFragment(new com.example.eventlotto.ui.MyEventsFragment());
+                    return true;
+                } else if (id == R.id.nav_profile) {
+                    loadFragment(new com.example.eventlotto.ui.ProfileFragment());
+                    return true;
+                }
+                return false;
+            });
+            if (savedInstanceState == null) {
+                bottomNavigationView.setSelectedItemId(R.id.nav_home);
+            }
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction tx = fm.beginTransaction();
+        tx.replace(R.id.fragment_container, fragment);
+        tx.commit();
     }
 }
