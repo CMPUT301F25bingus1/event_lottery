d01e5bb7d7ec9117df2be123152ad0876ea97867
chiu4 <jadenchiu16@gmail.com>
2025-10-27 00:56:13 -0600

    Began implementing US 01.06.01, QR code scanning functionality.

--- app/src/main/java/com/example/eventlotto/ui/ScanFragment.java
+++ app/src/main/java/com/example/eventlotto/ui/ScanFragment.java
@@ -1,22 +1,74 @@
 package com.example.eventlotto.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
 import com.example.eventlotto.R;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
-public class ScanFragment extends Fragment {
+public class ScanFragment extends Fragment implements BarcodeCallback {
+
+    private DecoratedBarcodeView barcodeView;
+    private boolean scanned = false;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        View view = inflater.inflate(R.layout.fragment_scan, container, false);
+        barcodeView = view.findViewById(R.id.barcode_scanner);
+
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startScanner();
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA}, 1001);
+        }
+
+        return view;
+    }
+
+    private void startScanner() {
+        barcodeView.decodeContinuous(this);
+        barcodeView.resume();
+    }
+
+    @Override
+    public void barcodeResult(BarcodeResult result) {
+        if (!scanned && result.getText() != null) {
+            scanned = true;
+            Bundle bundle = new Bundle();
+            bundle.putString("result", result.getText());
+
+            MyEventsFragment fragment = new MyEventsFragment();
+            fragment.setArguments(bundle);
+
+            requireActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+            // TODO: Properly implement QR scan result opens corresponding event tab
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (barcodeView != null) barcodeView.resume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (barcodeView != null) barcodeView.pause();
     }
 }
-
