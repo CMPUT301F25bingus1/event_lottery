025e450063df34c570c0a252b24295ce4764a78f
salian97 <salian@ualberta.ca>
2025-11-07 16:02:46 -0700

    fixed gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,14 +65,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.12.0")
     testImplementation("org.mockito:mockito-inline:5.2.0")
-    testImplementation("com.google.android.gms:play-services-tasks:18.2.0")
-
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:5.5.0")
-    testImplementation("org.mockito:mockito-inline:5.2.0")
     testImplementation("org.robolectric:robolectric:4.11.1")
 
     // Android Instrumentation / UI tests
@@ -89,6 +81,5 @@ dependencies {
 
     // Fix duplicate protobuf issue
     implementation("com.google.protobuf:protobuf-javalite:3.25.5")
-    testImplementation ("androidx.test:core:1.5.0")
-
 }
+
